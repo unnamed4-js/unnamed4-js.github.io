6ae832696ea8d1ff4d32ac6118c783d19351ef38
unnamed4-js <66266054+unnamed4-js@users.noreply.github.com>
2020-07-21 01:11:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //index.js 
 function sendEmail() { 	
-Email.send({ 	
-Host: "smtp.mailtrap.io", 	
+Email.send({
+SecureToken : "3f921fff-4a00-40b9-91b3-8e940c8601fb",
+Host : "smtp.mailtrap.io", 	
 Username : "2afdfbf79a4d1f", 	
 Password : "e9c7aa9d16e4ea", 	
 To : 'helium@mailnesia.com', 	
