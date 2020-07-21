722853a136c6c2f8e93fc8f330f8da53223bf3d5
unnamed4-js <66266054+unnamed4-js@users.noreply.github.com>
2020-07-21 02:28:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 //index.js 
-function sendEmail() { 	
-Email.send({
-SecureToken : "3f921fff-4a00-40b9-91b3-8e940c8601fb",
-Host : "smtp.mailtrap.io", 	
-Username : "2afdfbf79a4d1f", 	
-Password : "e9c7aa9d16e4ea", 	
-To : 'helium@mailnesia.com', 	
-From : "02a0b0fb8f-986fde@inbox.mailtrap.io", 	
-Subject : "IX", 	
-Body : "BODY TEXT", 	}).then( 		message => alert("mail sent successfully") 	
-  ); 
-}
+
+Email.send({ SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812", 
+To : 'tec@mailnesia.com', From : "this@isp.com", Subject : "This is the subject", Body : "And this is the body" }).then( message => alert(message) );
+
+
+
+// function sendEmail() { 	
+// Email.send({
+// SecureToken : "3f921fff-4a00-40b9-91b3-8e940c8601fb",
+// Host : "smtp.mailtrap.io", 	
+// Username : "2afdfbf79a4d1f", 	
+// Password : "e9c7aa9d16e4ea", 	
+// To : 'helium@mailnesia.com', 	
+// From : "02a0b0fb8f-986fde@inbox.mailtrap.io", 	
+// Subject : "IX", 	
+// Body : "BODY TEXT", 	}).then( 		message => alert("mail sent successfully") 	
+//   ); 
+// }
