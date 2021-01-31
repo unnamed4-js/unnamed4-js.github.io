4ec36e0c13799e9897fbe06dd72707ccbaf7b63b
unnamed4-js <66266054+unnamed4-js@users.noreply.github.com>
2021-01-31 18:16:46 +0100

    Create xhr.js

---
+++ xhr.js
@@ -0,0 +1,7 @@
+let xhr = new XMLHttpRequest();
+xhr.open('get', 'http://api.scrapestack.com/scrape?access_key=97f595b59788a92ebfe408938f35a1d7&url=https://proxyscrape.com/free-proxy-list');
+xhr.send();
+
+xhr.onload = function() {
+    console.log(xhr.response);
+};
